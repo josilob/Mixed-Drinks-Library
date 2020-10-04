96f5a0229c617835194bc20b73dde6aeba864110
josilob <bjosilo@gmail.com>
2020-10-04 19:58:25 -0400

    functionality achieved

--- src/Components/Cocktail.js
+++ src/Components/Cocktail.js
@@ -6,7 +6,56 @@ function Cocktail(props) {
 
 	const urlBase = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 
-	return;
+	const readDrink = (idDrink) => {
+		fetch(`${urlBase}${idDrink}`)
+			.then((response) => response.json())
+			.then((data) => {
+				setDetails(data.drinks[0]);
+			});
+	};
+
+	React.useEffect(() => {
+		readDrink(props.location.state.idDrink);
+	}, []);
+
+	React.useEffect(() => {
+		listIngr();
+		console.log('details log', details);
+	}, [details]);
+
+	const listIngr = () => {
+		let ingr = [];
+		for (let i = 1; i < 16; i++) {
+			console.log('New string log', details[`strIngredient${i}`]);
+			if (details[`strIngredient${i}`] == null) break;
+			ingr.push({
+				ingredientName: details[`strIngredient${i}`],
+				ingredientMeasure: details[`strMeasure${i}`],
+			});
+		}
+		setIngredients(ingr);
+		console.log('ingredients', ingr);
+	};
+
+	return (
+		<div className='Cocktail'>
+			<h1>{details.strDrink}</h1>
+			<img class='drink-img' src={details.strDrinkThumb} />
+			<div>
+				<div>
+					List of ingredients:
+					{ingredients.map((element, index) => (
+						<div key={index}>
+							{element.ingredientName}
+							{'  '}
+							{element.ingredientMeasure}
+						</div>
+					))}
+				</div>
+				<p>{details.strInstructions}</p>
+			</div>
+		</div>
+	);
 }
 
 export default Cocktail;
