617a8ea3bf459e04520a7ee10671df61bbb72ddc
josilob <bjosilo@gmail.com>
2020-10-09 00:57:22 -0400

    bottle names

--- src/Components/Drinks.js
+++ src/Components/Drinks.js
@@ -26,63 +26,76 @@ function Drinks() {
 			</div>
 			<div className='bottles'>
 				<div className='bottle-div'>
+					<h3 className='drink-type' id='vodka'>
+						VODKA
+					</h3>
 					<img
 						onClick={() => filterDrink('vodka')}
 						className='Bottle'
 						src='https://d3czfiwbzom72b.cloudfront.net/wp-content/uploads/2018/09/8686-ABSOLUT-VODKA-w.png'
 						alt='bottle'
 					/>
-					{/* <span className='drink-type'>VODKA</span> */}
 				</div>
 
 				<div className='bottle-div'>
+					<h3 className='drink-type' id='gin'>
+						GIN
+					</h3>
 					<img
 						onClick={() => filterDrink('gin')}
 						className='Bottle'
 						src='https://www.houseoftownend.com/Content/Images/Products/BEEF005.png'
 						alt='bottle'
 					/>
-					{/* <span className='drink-type'>GIN</span> */}
 				</div>
 
 				<div className='bottle-div'>
+					<h3 className='drink-type' id='tequila'>
+						TEQUILA
+					</h3>
 					<img
 						onClick={() => filterDrink('tequila')}
 						className='Bottle'
 						src='https://cdn.shopify.com/s/files/1/1531/2799/products/JoseCuervo_Reposado_fae752a8-6806-4ffc-9e71-59d4afa57524_grande.png?v=1597638393'
 						alt='bottle'
 					/>
-					{/* <span className='drink-type'>TEQUILA</span> */}
 				</div>
 
 				<div className='bottle-div'>
+					<h3 className='drink-type' id='bourbon'>
+						BOURBON
+					</h3>
 					<img
 						onClick={() => filterDrink('bourbon')}
 						className='Bottle'
 						src='https://d3czfiwbzom72b.cloudfront.net/wp-content/uploads/2018/09/7735-MAKERS-MARK-copy-w.png'
 						alt='bottle'
 					/>
-					{/* <span className='drink-type'>BOURBON</span> */}
 				</div>
 
 				<div className='bottle-div'>
+					<h3 className='drink-type' id='rum'>
+						RUM
+					</h3>
 					<img
 						onClick={() => filterDrink('rum')}
 						className='Bottle'
 						src='https://www.houseoftownend.com/Content/Images/Products/BACA020.png'
 						alt='bottle'
 					/>
-					{/* <span className='drink-type'>RUM</span> */}
 				</div>
 
 				<div className='bottle-div'>
+					<h3 className='drink-type' id='scotch'>
+						SCOTCH
+					</h3>
+
 					<img
 						onClick={() => filterDrink('scotch')}
 						className='Bottle'
 						src='https://d3czfiwbzom72b.cloudfront.net/wp-content/uploads/2018/09/002464-DEWAR-S-WHITE-LABEL-BLENDED-SCOTCH-w.png'
 						alt=''
 					/>
-					{/* <span className='drink-type'>SCOTCH</span> */}
 				</div>
 			</div>
 			<div className='drink-list'>
