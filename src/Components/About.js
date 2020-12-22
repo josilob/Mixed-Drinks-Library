99f4560c838c20393fd6ea55eb3c9f9461d74360
josilob <bjosilo@gmail.com>
2020-12-22 16:47:31 -0500

    about page + img folder

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,28 +6,21 @@ function About() {
 		<div className='About'>
 			<h4 className='about-text'>About the project:</h4>
 
-			<p className='about-text'> Student name: Bojan Josilo</p>
-
 			<p className='about-text'>
-				React based page combined with TheCocktialDB API.
+				Project made with React and TheCocktailDB API
 			</p>
 
 			<p className='about-text'>
-				Product of the sixth week at Remote Software Engineering Immersive
-				Coding Bootcamp at:
+				Created during the second week of studying React. This project is an
+				example of applying functional components and React Hooks as a new
+				features.
 			</p>
-			<img
-				className='ga-logo'
-				src='https://info.mercer.com/rs/521-DEV-513/images/General-Assembly-logo.png'
-				alt='img'
-			/>
+
 			<p className='about-text'>
 				Cocktail page filters mixed drinks by pivotal spirit and displays
 				possible options. After clicking one of the cocktails, it further
 				displays image with necessary ingredients, measurements and preparation
 				steps.
-				<br />
-				Date of presentation: 10-09-2020
 			</p>
 		</div>
 	);
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Nav from './Nav';
-import { Link } from 'react-router-dom';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
 	return (
